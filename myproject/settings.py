3f79573e750f149bdc340b7ce79105eb78e496dc
zaiba0 <shahzaib092123@gmail.com>
2024-12-06 14:18:47 +0530

    User Registration Form

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp.apps.MyappConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',# Add this for Bootstrap 4 support
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +133,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
