ecedd9e47dc0828cb320d1e67a8ac73a37b492a3
zaiba0 <shahzaib092123@gmail.com>
2024-12-09 17:38:17 +0530

    User Login System

--- myapp/models.py
+++ myapp/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')  # Specify related_name
 
     def __str__(self):
         return self.title
